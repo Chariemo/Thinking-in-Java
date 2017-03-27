8c18138391bec0e279dcad1c50a75cd11feab76c
Chariemo <charleyschen@163.com>
2017-03-28 00:40:52 +0800

    HashMap
    Signed-off-by: Chariemo <charleyschen@163.com>

---
+++ src/containers/Individual.java
@@ -0,0 +1,61 @@
+package containers;
+
+public class Individual implements Comparable<Individual> {
+	private static long  counter = 0;
+	private long id = counter++;
+	private String name;
+	public Individual(String name) {
+		this.name = name;
+	}
+	public Individual() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String toString() {
+		return getClass().getSimpleName() + (name == null ? "" : " " + name);
+	}
+	
+	public long id() {
+		return id;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return obj instanceof Individual && id == ((Individual) obj).id;
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		int result = 17;
+		if (name != null) {
+			result = 37 * result + name.hashCode();
+		}
+		result = 37 * result + (int)id;
+		return result;
+	}
+	
+	@Override
+	public int compareTo(Individual o) {
+		// TODO Auto-generated method stub
+		String first = getClass().getSimpleName();
+		String argFirst = o.getClass().getSimpleName();
+		int firstCompare = first.compareTo(argFirst);
+		if (firstCompare != 0) {
+			return firstCompare;
+		}
+		if (name != null && o.name != null) {
+			int secondCompare = name.compareTo(o.name);
+			if (secondCompare != 0) {
+				return secondCompare;
+			}
+		}
+		return (o.id < id ? -1 : (o.id == id) ? 0 : 1);
+	}
+
+	
+	public static void main(String[] args) {
+		
+	}
+}
